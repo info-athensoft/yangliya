3d342bf2c37e8819696939e9acffa7e9295d4dbd
Athens <athens314@hotmail.com>
2019-02-04 08:35:54 -0500

    added school entity

---
+++ src/main/java/com/athensoft/school/SchoolClass.java
@@ -0,0 +1,103 @@
+package com.athensoft.school;
+
+import java.time.LocalDate;
+
+public class SchoolClass {
+	private long globalId;
+	private long classId;
+	private String classCode;
+	private int classType;
+	private int classLevel;
+	private int classClass;
+	private String className;
+	private String classOwner;
+	private LocalDate createDate;
+	private int maxPerson;
+	private String classDesc;
+	private int classStatus;
+	
+	public long getGlobalId() {
+		return globalId;
+	}
+	public void setGlobalId(long globalId) {
+		this.globalId = globalId;
+	}
+	public long getClassId() {
+		return classId;
+	}
+	public void setClassId(long classId) {
+		this.classId = classId;
+	}
+	public String getClassCode() {
+		return classCode;
+	}
+	public void setClassCode(String classCode) {
+		this.classCode = classCode;
+	}
+	public int getClassType() {
+		return classType;
+	}
+	public void setClassType(int classType) {
+		this.classType = classType;
+	}
+	public int getClassLevel() {
+		return classLevel;
+	}
+	public void setClassLevel(int classLevel) {
+		this.classLevel = classLevel;
+	}
+	public int getClassClass() {
+		return classClass;
+	}
+	public void setClassClass(int classClass) {
+		this.classClass = classClass;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public String getClassOwner() {
+		return classOwner;
+	}
+	public void setClassOwner(String classOwner) {
+		this.classOwner = classOwner;
+	}
+	public LocalDate getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(LocalDate createDate) {
+		this.createDate = createDate;
+	}
+	public int getMaxPerson() {
+		return maxPerson;
+	}
+	public void setMaxPerson(int maxPerson) {
+		this.maxPerson = maxPerson;
+	}
+	public String getClassDesc() {
+		return classDesc;
+	}
+	public void setClassDesc(String classDesc) {
+		this.classDesc = classDesc;
+	}
+	
+	public int getClassStatus() {
+		return classStatus;
+	}
+	public void setClassStatus(int classStatus) {
+		this.classStatus = classStatus;
+	}
+	@Override
+	public String toString() {
+		return "SchoolClass [globalId=" + globalId + ", classId=" + classId + ", classCode=" + classCode
+				+ ", classType=" + classType + ", classLevel=" + classLevel + ", classClass=" + classClass
+				+ ", className=" + className + ", classOwner=" + classOwner + ", createDate=" + createDate
+				+ ", maxPerson=" + maxPerson + ", classDesc=" + classDesc + ", classStatus=" + classStatus + "]";
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/athensoft/school/SchoolReview.java
@@ -0,0 +1,105 @@
+package com.athensoft.school;
+
+import java.util.Date;
+
+public class SchoolReview {
+public static final int UNKNOWN = 0;
+	
+	/**
+	 * the status of review, constant value, APPROVED = 1
+	 */
+	public static final int APPROVED = 1;
+	
+	/**
+	 * the status of review, constant value, PENDING = 2
+	 */
+	public static final int PENDING = 2;
+	
+	/**
+	 * the status of review, constant value, REJECTED = 3
+	 */
+	public static final int REJECTED = 3;
+	
+	private long globalId;
+	private String targetCode;
+	private String reviewUUID;
+	private long entityId;
+	private String entityCode;
+	private String acctName;
+	private String reviewer;
+	private Date reviewDateTime;
+	private int reviewStatus;
+	private String reviewContent;
+	
+	
+	public long getGlobalId() {
+		return globalId;
+	}
+	public void setGlobalId(long globalId) {
+		this.globalId = globalId;
+	}
+	public String getTargetCode() {
+		return targetCode;
+	}
+	public void setTargetCode(String targetCode) {
+		this.targetCode = targetCode;
+	}
+	public String getReviewUUID() {
+		return reviewUUID;
+	}
+	public void setReviewUUID(String reviewUUID) {
+		this.reviewUUID = reviewUUID;
+	}
+	public long getEntityId() {
+		return entityId;
+	}
+	public void setEntityId(long entityId) {
+		this.entityId = entityId;
+	}
+	public String getAcctName() {
+		return acctName;
+	}
+	public void setAcctName(String acctName) {
+		this.acctName = acctName;
+	}
+	public Date getReviewDateTime() {
+		return reviewDateTime;
+	}
+	public void setReviewDateTime(Date reviewDateTime) {
+		this.reviewDateTime = reviewDateTime;
+	}
+	public int getReviewStatus() {
+		return reviewStatus;
+	}
+	public void setReviewStatus(int reviewStatus) {
+		this.reviewStatus = reviewStatus;
+	}
+	public String getReviewContent() {
+		return reviewContent;
+	}
+	public void setReviewContent(String reviewContent) {
+		this.reviewContent = reviewContent;
+	}
+	
+	public String getReviewer() {
+		return reviewer;
+	}
+	public void setReviewer(String reviewer) {
+		this.reviewer = reviewer;
+	}
+	
+	public String getEntityCode() {
+		return entityCode;
+	}
+	public void setEntityCode(String entityCode) {
+		this.entityCode = entityCode;
+	}
+	@Override
+	public String toString() {
+		return "SchoolReview [globalId=" + globalId + ", targetCode=" + targetCode + ", reviewUUID=" + reviewUUID
+				+ ", entityId=" + entityId + ", entityCode=" + entityCode + ", acctName=" + acctName + ", reviewer="
+				+ reviewer + ", reviewDateTime=" + reviewDateTime + ", reviewStatus=" + reviewStatus
+				+ ", reviewContent=" + reviewContent + "]";
+	}
+	
+}
